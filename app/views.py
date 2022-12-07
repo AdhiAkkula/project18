574d015d5bb9032407a3f17d465e7b5653442724
AdhiAkkula <akkulaadinarayana@gmail.com>
2022-12-07 09:25:51 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def inherit(request):
+    return render(request,'inherit.html')
+
+def herit(request):
+    return render(request,'herit.html')
